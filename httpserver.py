c0c41a8e8f29af4b00d9337670d4c2b0aa7009bb
Draterr <benpooncc@outlook.com>
2024-01-26 16:09:29 +0800

    added mime type support

--- httpserver.py
+++ httpserver.py
@@ -33,6 +33,7 @@ def run():
                 decoded_list = decoded.split("\r\n")
                 res_code = parse_request(decoded_list)
                 conn.send(construct_response(res_code))
+                conn.close()
                 break
 
 def construct_response(res):
@@ -41,17 +42,21 @@ def construct_response(res):
     crlf = "\r\n"
     code, val_path, path = res
     val_path = val_path.group()
+    val_path = is_slash(val_path)
     match code:
         case 200:
             response_code = "200 OK"
-            output = get_file(val_path)
-            if is_slash(val_path):
-                content_length = os.stat(DIR + is_slash(val_path)).st_size + 10
-            else: 
-                content_length = os.stat(DIR + val_path).st_size + 10
-            #ToDo: change Content-Type based on file extension
-            final_response = f"{http_version} {response_code}{crlf}Content-Type: text/html;charset=UTF-8{crlf}Connection: close{crlf}Content-Length: {content_length}{crlf}{crlf}{output}"
-            return bytes(final_response, 'utf-8')
+            ext = re.findall("\.(html|php|js\b|aspx|htm|jpg|png|css|asp|json|jpeg|svg|ico|pdf|txt|xml)",val_path)[0]
+            available_content_types ={"js": "application/javascript", "html": "text/html","htm": "text/html", "css": "text/css", "jpg": "image/jpeg", "png": "image/png","svg":"image/svg+xml","ico":"image/vnd.microsoft.icon","json":"application/json","jpeg":"image/jpeg","pdf":"application/pdf","txt":"text/plain","xml":"application/xml"} 
+            content_type = available_content_types[ext]
+            output = get_file(val_path,content_type)
+            content_length = os.stat(DIR + val_path).st_size
+            if ext == "jpg" or "jpeg" or "png":
+                final_response = f"{http_version} {response_code}{crlf}Content-Type: {content_type};charset=UTF-8{crlf}Connection: close{crlf}Content-Length: {content_length}{crlf}{crlf}"
+                byte_response = bytes(final_response, 'utf-8') 
+                return byte_response + output
+            else:
+                return bytes(final_response + output, 'utf-8')
         case 400:
             response_code = "400 Bad Request"
             final_response = f"{http_version} {response_code}{crlf}Content-Type: text/html;charset=UTF-8{crlf}Connection: close{crlf}{crlf}<h1>Error Response</h1>\n<p>Error code: {code}</p>\n<p>Requested Path: {path}\n<p>Something is wrong with the request</p>"
@@ -67,16 +72,17 @@ def construct_response(res):
 
 def parse_request(req_list):
     valid_req_method = ['GET','POST','PATCH','PUT','DELETE','HEAD','CONNECT','OPTIONS','TRACE']
-    req_line = req_list[0].split(" ") # get the request line e.g "GET / HTTP/1.1"
-    req_method = req_line[0] #get the request method "GET,POST,etc"
-    req_path = req_line[1] #get the reqeust path "/,/index.html,etc"
-    req_ver = req_line[2] #get the HTTP version "HTTP/1.1,HTTP/2,etc"
+    req_line = req_list[0].split(" ")               # get the request line e.g "GET / HTTP/1.1"
+    req_method = req_line[0]                        # get the request method "GET,POST,etc"
+    req_path = req_line[1]                          # get the reqeust path "/,/index.html,etc"
+    req_ver = req_line[2]                           # get the HTTP version "HTTP/1.1,HTTP/2,etc"
     url_decoded_path = urllib.parse.unquote(req_path) #URL decode the path
     path_pat = re.compile("^((?:\/[a-zA-Z0-19\.\-_~!\$&'\(\)\*\+,;=:@]+)+)(\/?\?\w+\=\w+(?:&\w+\=\w+)*$)?|\/") # regex to match for a valid path
     path_match = path_pat.match(url_decoded_path)
     ver_pat = re.compile("^(HTTP\/[2-3])$|^(HTTP\/1)(?:\.[0,1])?$") # regex to match for a valid HTTP version
     ver_match = ver_pat.match(req_ver)
-    output = get_file(path_match.group())
+    ext = re.findall("\.(html|php|js\b|aspx|htm|jpg|png|css|asp|json|jpeg|svg|ico|pdf|txt|xml)",is_slash(path_match.group()))[0]
+    output = get_file(path_match.group(),ext)
     if  req_method not in valid_req_method or path_match is None or ver_match is None:
         return 400,path_match,url_decoded_path
     elif req_method != "GET":
@@ -86,12 +92,17 @@ def parse_request(req_list):
     else:
         return 200,path_match,url_decoded_path
     
-def get_file(file):
+def get_file(file,extension):
+    readperm = ""
     slash = is_slash(file)
     if slash:
         file = slash
+    if extension == "jpg" or "jpeg" or "png":
+        readperm = "rb"
+    else:
+        readperm = "r"
     try:
-        fp = open(DIR + file)
+        fp = open(DIR + file, readperm)
     except PermissionError:
         return "No Read Access to File"
     except FileNotFoundError:
@@ -102,6 +113,6 @@ def get_file(file):
 
 def is_slash(path):
     if path == "/":
-        path = "index.html"
+        path = "/index.html"
     return path 
 run()
